b3ab9d38ea24e6d02f515a0f5a48742abe50ab75
Ula Placek <ulaplacek@gmail.com>
2019-07-05 14:22:55 +0200

    jackson added to pom.xml

--- src/main/java/pl/ulapla/controller/BookController.java
+++ src/main/java/pl/ulapla/controller/BookController.java
@@ -3,6 +3,7 @@ package pl.ulapla.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.ulapla.model.Book;
 
 @RestController
 @RequestMapping("/books")
@@ -11,6 +12,14 @@ public class BookController {
     public String hello(){
         return "{hello: World}";
     }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook(){
+        return new Book(1L,"9788324631766","Thinking in Java",
+                "Bruce Eckel","Helion","programming");
+    }
+
+
 }
 
 
