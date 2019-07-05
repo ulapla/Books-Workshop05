61dc15c1c8d7a786332b0dbac5dd0b0ac9a6caae
Ula Placek <ulaplacek@gmail.com>
2019-07-05 15:03:24 +0200

    getAllBooks method added

--- src/main/java/pl/ulapla/controller/BookController.java
+++ src/main/java/pl/ulapla/controller/BookController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 import pl.ulapla.model.Book;
 import pl.ulapla.model.MemoryBookService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
@@ -25,6 +27,11 @@ public class BookController {
     public Book helloBook(){
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
+    }
+
+    @RequestMapping("/allBooks")
+    public List<Book> getAllBooks(){
+        return memoryBookService.getList();
     }
 
 
